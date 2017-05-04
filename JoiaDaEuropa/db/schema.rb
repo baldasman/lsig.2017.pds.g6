f7f5796057809b45bec1c5502a005b470d7b5af9
Diogo Fernandes <luisdiogo.fernandes@gmail.com>
2017-05-04 11:45:04 +0100

    [PG6-36]

--- JoiaDaEuropa/db/schema.rb
+++ JoiaDaEuropa/db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504092227) do
+ActiveRecord::Schema.define(version: 20170503102053) do
 
   create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.decimal  "price",         precision: 10
     t.datetime "delivery_date"
+    t.decimal  "price",         precision: 2
     t.string   "obs"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
