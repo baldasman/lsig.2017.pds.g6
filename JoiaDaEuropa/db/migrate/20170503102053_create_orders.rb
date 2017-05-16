3e041a8b1c700badaac816b141f4cc8e70e3c311
Pedro Santos <baldasman@gmail.com>
2017-05-16 13:59:39 +0100

    [PG6-17]

--- JoiaDaEuropa/db/migrate/20170503102053_create_orders.rb
+++ JoiaDaEuropa/db/migrate/20170503102053_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.0]
     def change
         create_table :orders do |t|
             t.datetime :delivery_date
-            t.decimal :price, precision: 2
+            t.decimal :price, precision: 15, scale: 2
             t.string :obs
 
             t.timestamps
